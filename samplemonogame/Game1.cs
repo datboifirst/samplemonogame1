0d14d8644bcd1960f110541678021566a2c67c18
First <First@LT-UDVJTST421D0>
2024-09-03 16:16:34 +0700

    git  commit "update playerJump"

--- samplemonogame/Game1.cs
+++ samplemonogame/Game1.cs
@@ -8,7 +8,7 @@ namespace samplemonogame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        int p = 12;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
